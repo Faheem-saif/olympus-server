3b7104e19e4963f3ba7a1940601f46789e9bba31
Faheem-saif <faheem000saif@gmail.com>
2024-05-28 11:15:50 +0500

    olympus

---
+++ app/Models/User.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Provider;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'birthday',
+        'genre',
+        'phone',
+        'country',
+        'password',
+        'state',
+        'city',
+        'bio',
+        'website',
+        'religion',
+        'birthplace',
+        'occupation',
+        'politic',
+        'facebook',
+        'twitter',
+        'rss',
+        'dribble',
+        'spotify',
+    ];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    public function providers()
+    {
+        return $this->hasMany(Provider::class,'user_id','id');
+    }
+}
